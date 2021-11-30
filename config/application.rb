d223a639168dac7f415e710c7530566fb6742de1
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-30 08:51:35 +0000

    add config/locales.rbの記載を変更し、モデムの日本語機能を有効にする

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ module DevExamRails6
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
     config.load_defaults 6.0
     config.generators do |g|
       g.assets false
