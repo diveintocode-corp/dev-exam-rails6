f4d310051e49dc833d0e0e32c0d416f0563147d0
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-30 08:31:01 +0000

    add モデルにvalidatesを設定

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,9 @@
 class Building < ApplicationRecord
+  validates :building_name, presence: true
+  validates :rent, numericality: true
+  validates :address, presence: true
+  validates :age_of_building, numericality: true
+  validates :remark, length: {minimum: 1}, allow_blank: true 
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,6 @@
 class NearestStation < ApplicationRecord
   belongs_to :building
+  validates :route_name, presence: true
+  validates :station_name, presence: true
+  validates :walking_minitues, numericality: true
 end
