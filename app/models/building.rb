264917f618a9feed2e0c55b98269380b59c57973
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-30 07:45:50 +0000

    add Neareest_stationコントローラーのshowアクション追加、show.html.erb修正

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,4 @@
 class Building < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
 end
