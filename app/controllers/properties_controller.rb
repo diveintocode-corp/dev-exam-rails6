ca1004986c985b0c0b92897b635b3d5fe1c6ff01
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-22 09:48:05 +0900

    indexページアクションとview作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
 
-  # GET /properties or /properties.json
   def index
     @properties = Property.all
   end
