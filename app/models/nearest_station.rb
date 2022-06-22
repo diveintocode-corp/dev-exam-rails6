bb44922779393bd90db5f107723ea73eb16a66bf
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-22 10:53:43 +0900

    バリデーション作成

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,7 @@
 class NearestStation < ApplicationRecord
   belongs_to :property, inverse_of: :nearest_stations
+
+  validates :route_name, presence: true, on: :update
+  validates :station_name, presence: true, on: :update
+  validates :time_walk, presence: true, on: :update
 end
