52fc90e02cee5d5fa55c1c31140547b29b497bf2
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-22 10:05:43 +0900

    プライベートへパラムス作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -41,13 +41,25 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_property
       @property = Property.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:property_name, :rent, :address, :age, :content)
+      params.require(:property).permit(
+        :property_name,
+        :rent,
+        :address,
+        :age,
+        :content,
+      nearest_stations_attributes: [
+        :route_name,
+        :station_name,
+        :time_walk,
+        :property_id,
+        :id,
+        :_destroy,
+        ],
+      )
     end
 end
